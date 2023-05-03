9445286b58c41da145983f1d99947028ce090856
Yufei Wang <yew569@cornell.edu>
2023-05-03 14:39:43 -0400

    Remove index col from dataset and edit first page

--- final_project.py
+++ final_project.py
@@ -4,10 +4,18 @@ st.markdown("# Practical Applications of Machine Learning (PAML)")
 
 #############################################
 
-st.markdown("### Final Project - <project title>")
+st.markdown("### Final Project - NBA Salary Prediction")
 
 #############################################
 
-st.markdown("Add some description about your project, team, etc")
+st.markdown("#### Description")
+st.markdown("""Build a machine-learning pipeline that can predict the salaries of NBA players based on their performance and background. 
+While many players hire agents to negotiate their contracts, there are still cases where players receive salaries that do not match their performance. 
+""")
+st.markdown("Our hope with this project is to provide players and agents with a tool that can help them estimate their salaries more accurately.")
+
+st.markdown("#### Team")
+st.markdown("Jiarong Chen (jc2924)")
+st.markdown("Yufei Wang (yw569)")
 
 st.markdown("Click **Explore Dataset** to get started.")
